a3ed17c939956dde0ce28fb2043b18de8f65fbf4
maintheme2 <oligarh20031@mail.ru>
2023-04-04 15:20:57 +0300

    :smiley:

---
+++ week11/task1.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient
+
+client = MongoClient('mongodb://localhost', 27017)
+
+restaurants = client['test']['restaurants']
+
+def display_Irish():
+    for restaurant in restaurants.find({'cuisine': 'Irish'}):
+        print(restaurant['name'])
+
+def display_Irish_and_Russian():
+    for restaurant in restaurants.find({"$or": [{'cuisine': 'Italian'}, {'cuisine': 'Russian'}]}):
+        print(restaurant['name'])
+
+def find_by_address(address: list):
+    street_name = address[0]
+    building = address[1]
+    street_number = address[2]
+    restaurant = restaurants.find_one({"address.street": street_name, 
+                                        "address.building": building, 
+                                        "address.zipcode": street_number})
+    print(restaurant['name'])
+
+print("Irish cuisine:")
+display_Irish()
+print("\n")
+print("Irish and Russian cuisine:")
+display_Irish_and_Russian()
+print("\n")
+print("Find by address:")
+find_by_address(['Prospect Park West', '284', '11215'])
